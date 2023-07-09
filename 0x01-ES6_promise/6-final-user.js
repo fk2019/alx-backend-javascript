70dab8824bb649c782462ef320b2266c1511e18c
fk2019 <fk2019@gmail.com>
2023-07-09 18:05:35 +0300

    Add 6 and 7 files

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const arr = [];
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((data) => data.forEach((result) => {
-      if (result.status === 'fulfilled') arr.push(result);
-      arr.push({ status: result.status, value: result.reason });
-    }));
+    .then((data) => {
+      const arr = [];
+      data.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          arr.push(result);
+        } else {
+          arr.push({ status: result.status, value: `Error: ${result.reason.message}` });
+        }
+      });
+      return arr;
+    });
 }
