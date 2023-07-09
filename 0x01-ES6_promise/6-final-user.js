a59eafcbad53d8c101f1aa55503439e08b3c2f0c
fk2019 <fk2019@gmail.com>
2023-07-09 17:39:58 +0300

    Add 6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const arr = [];
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((data) => data.forEach((result) => {
+      if (result.status === 'fulfilled') arr.push(result);
+      arr.push({ status: result.status, value: result.reason });
+    }));
+}
