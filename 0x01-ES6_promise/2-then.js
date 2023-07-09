0551d0e65492e982947ef18c8f2e6fdfa5af387e
fk2019 <fk2019@gmail.com>
2023-07-09 16:26:39 +0300

    Return object

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
 export default function handleResponseFromAPI(promise) {
+  const objct = { status: 200, body: 'Success' };
   return promise
-    .then(() => ({ status: 200, body: 'Success' }))
+    .then(() => objct)
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
