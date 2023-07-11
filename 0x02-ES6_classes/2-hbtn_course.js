02812a95019db88fca5a8e6a5a5cc5a338c8bf33
fk2019 <fk2019@gmail.com>
2023-07-11 20:16:32 +0300

    Add hbtn course

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,55 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (typeof students !== 'object') {
+      throw new TypeError('students must be an array');
+    }
+    students.forEach((item) => {
+      if (typeof item !== 'string') throw new TypeError('Name must be a string');
+    });
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (typeof students !== 'object') {
+      throw new TypeError('students must be an array');
+    }
+    students.forEach((item) => {
+      if (typeof item !== 'string') throw new TypeError('Name must be a string');
+    });
+    this._students = students;
+  }
+}
