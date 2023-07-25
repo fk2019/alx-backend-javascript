4baca93cf4f7a8d9e15f2d1e45eee4def299d99d
fk2019 <fk2019@gmail.com>
2023-07-25 18:55:49 +0300

    Fix linting

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!(set instanceof Set)) throw new TypeError('set must be a set');
   if (typeof startString !== 'string');
   if (!startString || !startString.length) return '';
-  let arr = [];
+  const arr = [];
   set.forEach((el) => {
     if (el.startsWith(startString) === true) arr.push(el.slice(startString.length));
   });
