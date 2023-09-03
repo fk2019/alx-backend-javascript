4ef6038a9c298e5b9074942b746a1bc5491cf43d
fk2019 <fk2019@gmail.com>
2023-09-03 21:24:49 +0300

    Add more ops

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -6,10 +6,10 @@ describe('calculateNumber', () => {
     it('sum if type is SUM', () => expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6));
   });
   describe('type === SUBTRACT', () => {
-    it('subtract if type is SUBTRACT', () => expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6));
+    it('subtract if type is SUBTRACT', () => expect(calculateNumber('SUBTRACT', 5.4, 3.5)).to.equal(1));
   });
   describe('type === DIVIDE', () => {
-    it('divide if type is DIVIDE', () => expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6));
-    it('error if round b is 0', () => expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6));
+    it('divide if type is DIVIDE', () => expect(calculateNumber('DIVIDE', 8.4, 4.3)).to.equal(2));
+    it('error if round b is 0', () => expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error'));
   });
 });
