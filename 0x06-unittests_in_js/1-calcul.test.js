09df6e4b1642b384eb5dda7ae8559d9d863b4def
francisk <47480814+fk2019@users.noreply.github.com>
2023-10-01 09:36:59 +0300

    Delete 0x06-unittests_in_js directory

--- 0x06-unittests_in_js/1-calcul.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul');
-
-describe('calculateNumber', () => {
-  it('sum if type is SUM', () => assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6));
-  it('subract if type is SUBTRACT', () => assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4));
-  it('divide if type is DIVIDE', () => assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2));
-  it('error if round b is 0', () => assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error'));
-});
