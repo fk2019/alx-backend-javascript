4eb0183640aead0e86ec842d9016b2c596930a11
fk2019 <fk2019@gmail.com>
2023-11-23 20:18:03 +0300

    Update calculateNumber func

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,19 +1,16 @@
-function calculateNumber(type, a, b) {
-  const n1 = Math.round(a);
-  const n2 = Math.round(b);
-
+const calculateNumber = (type, a, b) => {
+  const a1 = Math.round(a);
+  const b1 = Math.round(b);
+  if (type === 'SUM') {
+    return a1 + b1;
+  }
   if (type === 'SUBTRACT') {
-    return n1 - n2;
+    return a1 - b1;
   }
-
   if (type === 'DIVIDE') {
-    if (n2 === 0) {
-      return 'Error';
-    }
-    return n1 / n2;
+    return b1 === 0 ? 'Error' : a1 / b1;
   }
-
-  return n1 + n2;
-}
+  return (0);
+};
 
 module.exports = calculateNumber;
