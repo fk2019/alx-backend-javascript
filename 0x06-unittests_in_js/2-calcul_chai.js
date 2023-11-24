f36784dd08d5b206a8432d48feb3da5ac7f0160c
fk2019 <fk2019@gmail.com>
2023-11-24 22:45:03 +0300

    Edit calc func

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,16 +1,14 @@
 const calculateNumber = (type, a, b) => {
-  const a1 = Math.round(a);
-  const b1 = Math.round(b);
   if (type === 'SUM') {
-    return a1 + b1;
+    return Math.round(a) + Math.round(b);
   }
   if (type === 'SUBTRACT') {
-    return a1 - b1;
+    return Math.round(a) - Math.round(b);
   }
   if (type === 'DIVIDE') {
-    return b1 === 0 ? 'Error' : a1 / b1;
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
   }
-  return (0);
+  return 0;
 };
 
 module.exports = calculateNumber;
